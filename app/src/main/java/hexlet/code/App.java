7a274dcedf0d0065d6c371d8905a6d09479e4cbf
Sergey Raklov <ra1976@inbox.ru>
2022-07-17 09:40:51 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
