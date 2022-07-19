1045a0377f8bf3abd097b37c20cdd36fa2847c8e
Sergey Raklov <ra1976@inbox.ru>
2022-07-19 13:31:54 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,5 +3,8 @@ package hexlet.code;
 class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String name = Cli.getName();
+        System.out.println("Hello, " + name);
     }
 }
