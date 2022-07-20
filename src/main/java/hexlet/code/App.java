9404278e75e42255957690473e598fe1fb08c453
Sergey Raklov <ra1976@inbox.ru>
2022-07-20 15:55:36 +0300

    update App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,10 +1,52 @@
 package hexlet.code;
 
-class App {
+import java.util.Scanner;
+
+public class App {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        String name = Cli.getName();
-        System.out.println("Hello, " + name);
+        Scanner words = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n2 - Even\n0 - Exit");
+        String choice = words.nextLine();
+        System.out.println("Your choice: " + choice);
+
+        switch (choice) {
+            case "1":
+                System.out.println("Welcome to the Brain Games!");
+                System.out.println("May I have your name?");
+                String name1 = words.nextLine();
+                System.out.println("Hello, " + name1);
+                break;
+            case "2":
+                System.out.println("Welcome to the Brain Games!");
+                System.out.println("May I have your name?");
+                String name2 = words.nextLine();
+                System.out.println("Hello, " + name2);
+                System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+                var count = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    var rand = (int) (19 * Math.random());
+                    System.out.println("Question: " + rand);
+                    var answer = words.nextLine();
+                    if (rand % 2 == 0 && answer.equalsIgnoreCase("yes")) {
+                        System.out.println("Correct!");
+                        count += 1;
+
+                    } else if (rand % 2 != 0 && answer.equalsIgnoreCase("no")) {
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'./nLet's try again, Bill!");
+                    }
+                }
+                if (count == 3) {
+                    System.out.println("Congratulations, " + name2);
+                }
+
+                break;
+
+        }
+
     }
 }
