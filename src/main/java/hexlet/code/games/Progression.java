2f18b8e23fe169307a67d467c26eb95aad958dd3
Sergey Raklov <ra1976@inbox.ru>
2022-07-22 17:59:55 +0300

    add Progression.java

---
+++ src/main/java/hexlet/code/games/Progression.java
@@ -0,0 +1,15 @@
+package hexlet.code.games;
+
+import java.util.Scanner;
+
+public class Progression {
+    public static void arithmeticProgressionNumbers() {
+        Scanner words = new Scanner(System.in);
+        Greet.getGreet();
+        String getName = Greet.name;
+
+        System.out.println("What number is missing in the progression?");
+        var count = 0;
+
+    }
+}
