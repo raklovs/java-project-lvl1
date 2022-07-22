ca8721388a6783ff8128617cc21fd2d0985bcdd2
Sergey Raklov <ra1976@inbox.ru>
2022-07-22 09:58:26 +0300

    add GCD.java

---
+++ src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,15 @@
+package hexlet.code.games;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void GreatestCommonDivisor() {
+        Scanner words = new Scanner(System.in);
+        Greet.getGreet();
+        String getName = Greet.name;
+
+        System.out.println("What is the result of the expression?");
+        var count = 0;
+        
+    }
+}
