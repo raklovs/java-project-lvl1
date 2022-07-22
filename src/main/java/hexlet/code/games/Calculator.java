bb865f9289df21a98d36cc94b0cb35ce9e70d8fb
Sergey Raklov <ra1976@inbox.ru>
2022-07-22 09:06:50 +0300

    update Calculator.java

--- src/main/java/hexlet/code/games/Calculator.java
+++ src/main/java/hexlet/code/games/Calculator.java
@@ -1,4 +1,5 @@
 package hexlet.code.games;
+
 import java.util.Scanner;
 
 public class Calculator {
@@ -10,6 +11,47 @@ public class Calculator {
 
         System.out.println("What is the result of the expression?");
         var count = 0;
+        var result = 0;
+        var operand = "";
+
+        for (int i = 0; i < 3; i++) {
+            var randomNumber1 = (int) (17 * Math.random());
+            var randomNumber2 = (int) (17 * Math.random());
+            var switchNumbers = (int) (3 * Math.random());
+
+
+            switch (switchNumbers) {
+                case 0:
+                    result = randomNumber1 + randomNumber2;
+                    operand = "+";
+                    break;
+                case 1:
+                    result = randomNumber1 * randomNumber2;
+                    operand = "*";
+                    break;
+                case 2:
+                    result = randomNumber1 / randomNumber2;
+                    operand = "/";
+                    break;
+            }
+            System.out.println("Question: " + randomNumber1 + " " + operand + " " + randomNumber2);
+            String choice = words.nextLine();
+            System.out.println("Your answer: " + choice);
+
+            if (String.valueOf(result).equals(choice)) {
+                System.out.println("Correct!");
+                count++;
+            } else {
+                System.out.println(choice + " is wrong answer ;(. Correct answer was " + result + ".");
+                System.out.println("Let's try again, " + getName + "!");
+                break;
+            }
+
+        }
+        if (count == 3) {
+            System.out.println("Congratulations, " + getName + "!");
+        }
+
     }
 
 }
