4cdddd5d57a23b25913704b68cbfeeea7bd67026
Sergey Raklov <ra1976@inbox.ru>
2022-07-24 13:32:09 +0300

    update src/main/java/hexlet/code/games/Prime.java

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Prime {
 
     public static boolean isPrime(int number) {
-        if (number <= 1) {
+        if (number > 1) {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
             return false;
         }
-        for (int i = 2; i < number / 2; i++) {
-            if ((number % i) == 0)
-                return false;
-        }
-        return true;
     }
+
 
 
     public static void PrimeNumber() {
