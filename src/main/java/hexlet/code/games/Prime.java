f4f711383f44a3f07898b76d20e98c46b9ea1c4d
Sergey Raklov <ra1976@inbox.ru>
2022-07-24 14:58:24 +0300

    update src/main/java/hexlet/code/games/Prime.java

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -4,20 +4,17 @@ import java.util.Scanner;
 
 public class Prime {
 
-    public static boolean isPrime(int number) {
-        if (number > 1) {
-            for (int i = 2; i < number; i++) {
-                if (number % i == 0) {
-                    return false;
-                }
-            }
-            return true;
-        } else {
+    private static boolean isPrime(final int number) {
+        if (number <= 1) {
             return false;
         }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-
-
 
     public static void PrimeNumber() {
         Scanner words = new Scanner(System.in);
@@ -29,23 +26,33 @@ public class Prime {
         for (var i = 0; i < 3; i++) {
             int randomNumber = (int) (Math.random() * 10) + 1;
             System.out.println("Question: " + randomNumber);
+            String result = "";
 
             boolean isPrime = isPrime(randomNumber);
             Scanner answerScan = new Scanner(System.in);
             String answer = answerScan.nextLine();
             System.out.println("Your answer: " + answer);
 
-            if (isPrime & answer.equals("yes")) {
+            if (isPrime) {
+                result = "yes";
+            } else {
+                result = "no";
+            }
+
+            if (isPrime && answer.equals("yes")) {
                 System.out.println("Correct!");
+                result = "yes";
                 count++;
-            } else if (!isPrime & answer.equals("no")) {
+            } else if (!isPrime && answer.equals("no")) {
                 System.out.println("Correct!");
+                result = "no";
                 count++;
             } else {
-                System.out.println("'" + answer + "'" + " is wrong answer ;(. " + "Correct answer was 'no'.");
+                System.out.println(answer + " is wrong answer ;(. Correct answer was " + result + ".");
                 System.out.println("Let's try again, " + getName + "!");
                 break;
             }
+
         }
         if (count == 3) {
             System.out.println("Congratulations, " + getName + "!");
