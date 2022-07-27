4c754909f8b1975d1fa6adb587b5462543a01213
Sergey Raklov <ra1976@inbox.ru>
2022-07-27 13:24:37 +0300

    update Engine.java

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -1,51 +1,26 @@
 package hexlet.code;
-import java.util.Scanner;
-import hexlet.code.games.Even;
-import hexlet.code.games.GCD;
+
+
 import hexlet.code.games.Greet;
-import hexlet.code.games.Prime;
-import hexlet.code.games.Progression;
-import hexlet.code.games.Calculator;
+
+import java.util.Scanner;
 
 public class Engine {
-    public static void gameManagement() {
+
+    private static String getAnswer;
+    public static final int NUMBER_OF_QUESTION = 3;
+
+    public static String getAnswer() {
+        return getAnswer;
+    }
+
+    public static void yourAnswer() {
         Scanner words = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calculator");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
-        String choice = (words.nextLine());
-        System.out.println("Your choice: " + choice);
+        getAnswer = words.nextLine();
+        System.out.println("Your answer: " + getAnswer);
+    }
 
-        switch (choice) {
-            case "0":
-                System.out.println("Goodbye!");
-                break;
-            case "1":
-                Greet.getGreet();
-                break;
-            case "2":
-                Even.guessEvenOrOddNumber();
-                break;
-            case "3":
-                Calculator.calculation();
-                break;
-            case "4":
-                GCD.findGreatestCommonDivisor();
-                break;
-            case "5":
-                Progression.arithmeticProgressionNumbers();
-                break;
-            case "6":
-                Prime.primeNumber();
-                break;
-            default:
-                System.out.println("You have entered a number outside the range.");
-        }
-
+    public static void congratulations() {
+        System.out.println("Congratulations, " + Greet.getName() + "!");
     }
 }
