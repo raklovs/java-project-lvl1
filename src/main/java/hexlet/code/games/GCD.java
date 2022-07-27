1928f5ab0f8f1d000a49dea7feb0e22ac4b363a5
Sergey Raklov <ra1976@inbox.ru>
2022-07-27 13:28:53 +0300

    update GCD.java

--- src/main/java/hexlet/code/games/GCD.java
+++ src/main/java/hexlet/code/games/GCD.java
@@ -1,6 +1,6 @@
 package hexlet.code.games;
 
-import java.util.Scanner;
+import hexlet.code.Engine;
 
 public class GCD {
 
@@ -12,37 +12,28 @@ public class GCD {
     }
 
     public static void findGreatestCommonDivisor() {
-        Scanner words = new Scanner(System.in);
+
         Greet.getGreet();
-        String getName = Greet.getName();
-
         System.out.println("Find the greatest common divisor of given numbers.");
-        var count = 0;
         final var multiplier = 100;
-        final int numberOfQuestions = 3;
 
-        for (int i = 0; i < numberOfQuestions; i++) {
+        for (int i = 0; i < Engine.NUMBER_OF_QUESTION; i++) {
             var randomNumber1 = (int) (multiplier * Math.random());
             var randomNumber2 = (int) (multiplier * Math.random());
             var answer = GCD.algorithmGreatestCommonDivisor(randomNumber1, randomNumber2);
 
             System.out.println("Question: " + randomNumber1 + " " + randomNumber2);
-            String choice = words.nextLine();
-            System.out.println("Your answer: " + choice);
+            Engine.yourAnswer();
 
-            if (String.valueOf(answer).equals(choice)) {
+            if (String.valueOf(answer).equals(Engine.getAnswer())) {
                 System.out.println("Correct!");
-                count++;
             } else {
-                System.out.println(choice + " is wrong answer ;(. Correct answer was " + answer + ".");
-                System.out.println("Let's try again, " + getName + "!");
+                System.out.println(Engine.getAnswer() + " is wrong answer ;(. Correct answer was " + answer + ".");
+                System.out.println("Let's try again, " + Greet.getName() + "!");
                 break;
             }
 
         }
-        if (count == numberOfQuestions) {
-            System.out.println("Congratulations, " + getName + "!");
-        }
-
+        Engine.congratulations();
     }
 }
