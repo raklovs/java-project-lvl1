4c4db559137f55cd951d05b0afd13e455d26c8b9
Sergey Raklov <ra1976@inbox.ru>
2022-07-29 13:20:47 +0300

    update Engine.java

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -1,11 +1,62 @@
 package hexlet.code;
 
-
-import hexlet.code.games.Greet;
-
 import java.util.Scanner;
 
+import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
+import hexlet.code.games.Greet;
+import hexlet.code.games.Prime;
+import hexlet.code.games.Progression;
+import hexlet.code.games.Calculator;
+
 public class Engine {
+
+    public static void gameManagement() {
+        Scanner words = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calculator");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        String choice = (words.nextLine());
+        System.out.println("Your choice: " + choice);
+
+        switch (choice) {
+            case "0" -> System.out.println("Goodbye!");
+            case "1" -> Greet.getGreet();
+            case "2" -> {
+                Greet.getGreet();
+                System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+                Even.guessEvenOrOddNumber();
+            }
+            case "3" -> {
+                Greet.getGreet();
+                System.out.println("What is the result of the expression?");
+                Calculator.calculation();
+            }
+            case "4" -> {
+                Greet.getGreet();
+                System.out.println("Find the greatest common divisor of given numbers.");
+                GCD.findGreatestCommonDivisor();
+            }
+            case "5" -> {
+                Greet.getGreet();
+                System.out.println("What number is missing in the progression?");
+                Progression.arithmeticProgressionNumbers();
+            }
+            case "6" -> {
+                Greet.getGreet();
+                System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+                Prime.primeNumber();
+            }
+            default -> System.out.println("You have entered a number outside the range.");
+        }
+
+    }
+
 
     private static String getAnswer;
     public static final int NUMBER_OF_QUESTION = 3;
