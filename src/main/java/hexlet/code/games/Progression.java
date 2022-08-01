e5d846bd9d78bb04395cf839d547888d51e41f09
Sergey Raklov <ra1976@inbox.ru>
2022-08-01 10:24:45 +0300

    update Progression.java

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -5,25 +5,23 @@ import java.util.Arrays;
 import hexlet.code.Engine;
 
 public class Progression {
+    static final int MULTIPLIER_1 = 10;
+    static final int MULTIPLIER_2 = 5;
+    static final int MULTIPLIER_3 = 9;
 
     public static void arithmeticProgressionNumbers() {
         String[][] response = new String[Engine.NUMBER_OF_QUESTION][2];
         String question = "What number is missing in the progression?";
 
-        final var multiplier1 = 10;
-        final var multiplier2 = 5;
-        final var multiplier3 = 9;
-        int i;
-
-        for (i = 0; i < Engine.NUMBER_OF_QUESTION; i++) {
-            var beginNumber = (int) (multiplier1 * Math.random() - 1);
-            var stepNumber = (int) (multiplier2 * Math.random() + 1);
+        for (int i = 0; i < Engine.NUMBER_OF_QUESTION; i++) {
+            var beginNumber = (int) (MULTIPLIER_1 * Math.random() - 1);
+            var stepNumber = (int) (MULTIPLIER_2 * Math.random() + 1);
             final var arrayLength = 10;
             int[] progression = new int[arrayLength];
             String[] arrayOfString = new String[progression.length];
             int rezult = beginNumber;
             String replacement = "..";
-            int randomReplacement = ((int) (multiplier3 * Math.random()));
+            int randomReplacement = ((int) (MULTIPLIER_3 * Math.random()));
             int randomChoice = 0;
 
             for (int j = 0; j < progression.length; j++) {
