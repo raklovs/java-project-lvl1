feb8dfd5e2a3edcd5fc325047f2f820e9aedd89a
Sergey Raklov <ra1976@inbox.ru>
2022-08-01 10:23:24 +0300

    update Greet.java

--- src/main/java/hexlet/code/games/Greet.java
+++ src/main/java/hexlet/code/games/Greet.java
@@ -4,17 +4,11 @@ import java.util.Scanner;
 
 public class Greet {
 
-    private static String name;
-
-    public static String getName() {
-        return name;
-    }
-
     public static void getGreet() {
         Scanner words = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
-        name = words.nextLine();
+        String name = words.nextLine();
         System.out.println("Hello, " + name + "!");
     }
 }
