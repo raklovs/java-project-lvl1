b0e2b94ef63d78d3916d162bf3dd583df940e44d
Sergey Raklov <ra1976@inbox.ru>
2022-08-01 14:03:03 +0300

    update Progression.java

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -5,23 +5,22 @@ import java.util.Arrays;
 import hexlet.code.Engine;
 
 public class Progression {
-    static final int MULTIPLIER_1 = 10;
-    static final int MULTIPLIER_2 = 5;
-    static final int MULTIPLIER_3 = 9;
+
+    static final int PROGRESS_SIZE = 10;
 
     public static void arithmeticProgressionNumbers() {
         String[][] response = new String[Engine.NUMBER_OF_QUESTION][2];
         String question = "What number is missing in the progression?";
 
         for (int i = 0; i < Engine.NUMBER_OF_QUESTION; i++) {
-            var beginNumber = (int) (MULTIPLIER_1 * Math.random() - 1);
-            var stepNumber = (int) (MULTIPLIER_2 * Math.random() + 1);
+            var beginNumber = Engine.getRandom(Engine.MAX_NUMBER);
+            var stepNumber = Math.abs(Engine.getRandom(Engine.MAX_NUMBER)) + 1;
             final var arrayLength = 10;
             int[] progression = new int[arrayLength];
             String[] arrayOfString = new String[progression.length];
             int rezult = beginNumber;
             String replacement = "..";
-            int randomReplacement = ((int) (MULTIPLIER_3 * Math.random()));
+            int randomReplacement = Engine.getRandom(PROGRESS_SIZE);
             int randomChoice = 0;
 
             for (int j = 0; j < progression.length; j++) {
