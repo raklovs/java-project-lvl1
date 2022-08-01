1eb822b46f8c6964838491d2d9b36ab1e9a80d0e
Sergey Raklov <ra1976@inbox.ru>
2022-08-01 14:01:25 +0300

    update Even.java

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -4,15 +4,13 @@ import hexlet.code.Engine;
 
 public class Even {
 
-    static final int MULTIPLIER = 17;
-
     public static void guessEvenOrOddNumber() {
 
         String[][] response = new String[Engine.NUMBER_OF_QUESTION][2];
         String question = "Answer 'yes' if number even otherwise answer 'no'.";
 
         for (int i = 0; i < Engine.NUMBER_OF_QUESTION; i++) {
-            var randomNumber = (int) (MULTIPLIER * Math.random());
+            int randomNumber = Engine.getRandom(Engine.MAX_NUMBER);
             response[i][0] = Integer.toString(randomNumber);
             response[i][1] = randomNumber % 2 == 0 ? "yes" : "no";
 
