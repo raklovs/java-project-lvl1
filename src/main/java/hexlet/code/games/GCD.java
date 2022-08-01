82c588c7e443de9efb99fbb20d48966cc1afee3d
Sergey Raklov <ra1976@inbox.ru>
2022-08-01 14:02:01 +0300

    update GCD.java

--- src/main/java/hexlet/code/games/GCD.java
+++ src/main/java/hexlet/code/games/GCD.java
@@ -4,8 +4,6 @@ import hexlet.code.Engine;
 
 public class GCD {
 
-    static final int MULTIPLIER = 100;
-
     public static int algorithmGreatestCommonDivisor(int number1, int number2) {
         if (number2 == 0) {
             return number1;
@@ -18,8 +16,8 @@ public class GCD {
         String question = "Find the greatest common divisor of given numbers.";
 
         for (int i = 0; i < Engine.NUMBER_OF_QUESTION; i++) {
-            var randomNumber1 = (int) (MULTIPLIER * Math.random());
-            var randomNumber2 = (int) (MULTIPLIER * Math.random());
+            var randomNumber1 = Engine.getRandom(Engine.MAX_NUMBER);
+            var randomNumber2 = Engine.getRandom(Engine.MAX_NUMBER);
             var answer = GCD.algorithmGreatestCommonDivisor(randomNumber1, randomNumber2);
 
             response[i][0] = randomNumber1 + " " + randomNumber2;
